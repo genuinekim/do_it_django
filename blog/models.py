6dbb8e8b046eff3698563e1be85405186e2736b6
Kim SeongJin <ksz1860@naver.com>
2023-01-27 10:23:55 +0900

    add file_upload Field to Post model

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,11 @@ class Post(models.Model):
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
